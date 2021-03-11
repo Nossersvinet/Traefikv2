a7eae08071400ede50258819b0fcd2001e811b93
doob187 <60312740+doob187@users.noreply.github.com>
2021-03-11 20:50:45 +0100

    Update gpu.sh

--- templates/local/gpu.sh
+++ templates/local/gpu.sh
@@ -27,3 +27,65 @@ while true; do
      echo "lshw not found" && break
   fi
 done
+
+##IGPU
+
+##NVIDIA
+nvidiarepo() {
+CHK=$(cat /etc/apt/sources.list.d/nvidia-docker.list | grep -qE nvidia && echo true || echo false)
+if [[ $CHK == "false" ]]; then
+   curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+     sudo apt-key add -
+   distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+   curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+     sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+fi
+}
+
+nvidiainstall{} (
+CHKNV=$(command -v nvidia-smi)
+if [[ ! -x "$CHKNV" ]]; then
+   package_list="update upgrade nvidia-container-toolkit nvidia-container-runtime"
+   for i in ${package_list}; do
+       apt $i -yqq 1>/dev/null 2>&1
+   done
+fi
+}
+
+dockerpart() {
+DCHK=$(cat /etc/docker/daemon.json | grep -qE 'nvidia' && echo true || echo false)
+if [[ $DCHK == "false" ]]; then
+sudo tee /etc/docker/daemon.json <<EOF
+{
+    "default-runtime": "nvidia",
+    "runtimes": {
+        "nvidia": {
+            "path": "/usr/bin/nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+    }
+}
+EOF
+fi
+}
+
+dockerdreload() {
+DREA=$(pidof dockerd && echo true || echo false)
+if [[ $DREA == "true" ]]; then
+   pkill -SIGHUP dockerd
+fi
+}
+
+nvlastchk() {
+DREA=$(pidof dockerd && echo true || echo false)
+DCHK=$(cat /etc/docker/daemon.json | grep -qE 'nvidia' && echo true || echo false)
+CHKNV=$(command -v nvidia-smi)
+
+if [[ $DREA == "true" && $DCHK == "true" && $CHKNV == "true" ]]; then
+   echo " nvidia-container-runtime is working"
+else
+   echo " nvidia-container-runtime is not working"
+fi
+}
+
+
