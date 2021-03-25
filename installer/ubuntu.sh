dac38453cd28d97f7cc8bca0bed6a8e9eab0fa82
doob187 <60312740+doob187@users.noreply.github.com>
2021-03-25 21:28:54 +0100

    Fix TZ

--- installer/ubuntu.sh
+++ installer/ubuntu.sh
@@ -410,10 +410,9 @@ TZONE=$($(command -v timedatectl) | grep "Time zone:" | awk '{print $3}')
 if [[ $TZTEST != "false" ]];then
    if [[ $TZONE != "" ]];then
       if [[ -f $basefolder/compose/.env ]];then sed -i '/TZ=/d' $basefolder/compose/.env;fi
-          TZ=$TZONE 
-          #echo "TZ=${TZ}" >> $basefolder/compose/.env
-          grep -qE 'TZ=${TZ}' $basefolder/compose/.env || \
-               echo 'TZ=${TZ}' >> $basefolder/compose/.env
+      TZ=$TZONE 
+      grep -qE 'TZ=$TZONE' $basefolder/compose/.env || \
+           echo "TZ=$TZONE" >> $basefolder/compose/.env
    fi
 fi
 }
