d6809ecea469e06a69da0de7b81e19d30aa52f63
doob187 <60312740+doob187@users.noreply.github.com>
2021-04-02 13:12:55 +0200

    Create lxcstart.sh

---
+++ installer/.subinstall/lxcstart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Title:      LXC Bypass the mount :shared 
+# OS Branch:  ubuntu,debian,rasbian
+# Author(s):  mrdoob
+# Coauthor:   DrAgOn141
+# URL:        https://sudobox.io/
+# GNU:        General Public License v3.0
+################################################################################
+
+## command here below
