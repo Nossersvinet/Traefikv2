82d3087c8681d67f8f6550e497999f6cfc9598a1
doob187 <60312740+doob187@users.noreply.github.com>
2021-04-02 14:19:02 +0200

    Update lxc.sh

--- installer/.subinstall/lxc.sh
+++ installer/.subinstall/lxc.sh
@@ -20,10 +20,25 @@
 ## here the actions
 LXC() {
   if [[ ! -x $(command -v rsync) ]];then $(command -v apt) install --reinstall rsync -yqq 1>/dev/null 2>&1;fi
-     $(command -v rsync) /opt/traefik/installer/.subinstall/lxcstart.sh /root/lxcstart.sh -aq --info=progress2 -hv
-     $(command -v chmod) a+x /root/lxcstart.sh
+     $(command -v rsync) /opt/traefik/installer/.subinstall/lxcstart.sh /home/.lxcstart.sh -aq --info=progress2 -hv
+     $(command -v chmod) a+x /home/.lxcstart.sh
 ## set crontab
+cat <<EOF > /etc/cron.d/lxcstart
+@reboot root /home/.lxcstart.sh 1>/dev/null 2>&1
+EOF
+sleep 5
+tee <<-EOF
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+    ❌ INFO
+    please add follow features to your LXC Container
+    keyctl, nesting and fuse
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+  read -erp "Confirm Info | PRESS [ENTER]" typed </dev/tty
 
+
+clear && exit
+}
 
 
 if [[ "$(systemd-detect-virt)" == "lxc" ]];then
