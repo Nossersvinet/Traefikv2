9d99d816ed0baa8e77f858ce56f3683443cf054c
doob187 <60312740+doob187@users.noreply.github.com>
2021-04-02 13:02:04 +0200

    Update lxc.sh

--- installer/.subinstall/lxc.sh
+++ installer/.subinstall/lxc.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+#
+# Title:      LXC Bypass the mount :shared 
+# OS Branch:  ubuntu,debian,rasbian
+# Author(s):  mrdoob
+# Coauthor:   DrAgOn141
+# URL:        https://sudobox.io/
+# GNU:        General Public License v3.0
+################################################################################
+# shellcheck disable=SC2003
+# shellcheck disable=SC2006
+# shellcheck disable=SC2207
+# shellcheck disable=SC2012
+# shellcheck disable=SC2086
+# shellcheck disable=SC2196
+# shellcheck disable=SC2046
+#FUNCTIONS
+
 ## note
+## here the actions
